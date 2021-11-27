b0cd43a6bfa8f36c943bf8c54905c3a937cd6d12
綾雪 <2239301685@qq.com>
2021-11-27 12:51:20 +0800

    功能完善 bug修复....

--- Bankeralgorithm/Bankeralgorithm.cpp
+++ Bankeralgorithm/Bankeralgorithm.cpp
@@ -11,7 +11,7 @@ public:char name[20];
       int A;
       int B;
       int C;
-      bool flag1;                                                                           //操作标志位
+      bool flag1;                                                                           //操作标志位 没用到
       bool flag2;                                                                           //安全型算法标志位
       Resources(void);
       Resources(char n[20]);
@@ -67,7 +67,7 @@ void Resources::set(int a, int b, int c)
 {
     A = a, B = b, C = c;
 }
-void securityCheck(Resources avi, Resources PT[5], Resources A[5],Resources N[5],Resources Req)           
+void securityCheck(Resources avi, Resources PT[5], Resources A[5],Resources N[5],Resources Req,int size)           
 {
     int flag=0,Temp[5] = {0,0,0,0,0},count=0,j=1;
     Resources workCout[6];
@@ -91,23 +91,23 @@ void securityCheck(Resources avi, Resources PT[5], Resources A[5],Resources N[5]
                 break;
             }
             count++;
-            if (flag == 5)
+            if (flag == size)
                 break;
-            if (count > 10)
+            if (count > (size*2))
                 break;
-            i = (i+1) % 5;
+            i = (i+1) % size;
         }
-        if (count > 10)
+        if (count > (size*2))
         {
             cout << "SYSTEM INSECURITY" << endl;
             if(strcmp(Req.name,"NULL")!=0)
                 cout << Req.name << " CAN'T GET RESOURCES IMMEDIATELY" << endl;
             break;
         }
-        if (flag == 5)
+        if (flag == size)
         {
                 cout << "Name\t\tWork\t\tNeed\t\tAllocation\t\tWork+Allocation\t\tFinish" << endl;
-                for(int i=0;i<5;i++)
+                for(int i=0;i<size;i++)
                 {
                     cout << PT[Temp[i]].name << "\t\t" << workCout[i].A << "  " << workCout[i].B << "  " << workCout[i].C << "  ";
                     cout << "\t" << N[Temp[i]].A << "  " << N[Temp[i]].B << "  " << N[Temp[i]].C << "  ";
@@ -126,70 +126,79 @@ void securityCheck(Resources avi, Resources PT[5], Resources A[5],Resources N[5]
 }
 int main()
 {
-    int size = 5,cursor=-1;                                                                           //用于完善程序通用性
+    int size = 5,cursor=-1,ddd;                                                                           //用于完善程序通用性
     int MenuN=0;
-    bool flag = false;
+    bool flag = false,flag1=false;
     char AviName[20] = "availabe";
     char Te[5] = "NULL";
     char T[20],Tbeta[20];
     int Tn[6];
+    cout << "Input the type of the resources and number of customer"<<endl;
+    cin >> ddd >>  size;                                                                                 //没什么作用 形式主义 使之和要求相同
     Resources P[5],Allocation[5],Need[5],Reqtest,availabe(AviName);
     cout << "Input the amount of resource (maximum,allocated) of each customer" << endl;
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < size; i++)
     {
         cin >> T >> Tn[0] >> Tn[1] >> Tn[2] >> Tn[3] >> Tn[4] >> Tn[5];
         Resources A(T, Tn[0], Tn[1], Tn[2]);
         Resources B(T, Tn[3], Tn[4], Tn[5]);
         P[i] = A,Allocation[i]=B,Need[i]=A-B;
     }
+    Resources Pt[5], At[5], Nt[5],Avt;    
     cout << "Input the amount of resources(available)" << endl;
     cin >> Tn[0] >> Tn[1] >> Tn[2];
     availabe.set(Tn[0], Tn[1], Tn[2]);
     while (true)
     {
         cout << "1:judge the system security" << endl << "2:judge the request security" << endl << "3:exit" << endl;
+        Avt = availabe;
+        for (int i = 0; i < size; i++)
+    {
+        Pt[i] = P[i];
+        At[i] = Allocation[i];
+        Nt[i] = Need[i];
+    }
         cin >> MenuN;
         if (MenuN == 1)
         {
             Resources Temp(Te);
             Reqtest = Temp;
-            securityCheck(availabe, P, Allocation, Need, Reqtest);
-            break;
+            securityCheck(Avt, Pt, At, Nt, Reqtest,size);
         }
         else if (MenuN == 2)
         {
+            flag1 = false;
             cout << "Please input the customer's name and request"<<endl;
             cin >> Tbeta >> Tn[0] >> Tn[1] >> Tn[2];
             Resources Temp(Tbeta);
             Reqtest = Temp;
             Reqtest.set(Tn[0], Tn[1], Tn[2]);
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < size; i++)
             {
-                if (strcmp(P[i].name, Reqtest.name) == 0)
+                if (strcmp(Pt[i].name, Reqtest.name) == 0)
                 {
                     cursor = i;
                     flag = true;
-                    break;
                 }
             }
             if (flag)
             {
-                if (availabe.A < Reqtest.A && availabe.B < Reqtest.B && availabe.C < Reqtest.C)
+                if (Avt.A < Reqtest.A && Avt.B < Reqtest.B && Avt.C < Reqtest.C)
                     cout << "INSUFFICIENT AVAILABLE RESOURCES" << endl;
-                else if (Need[cursor].A < Reqtest.A && Need[cursor].B < Reqtest.B && Need[cursor].C < Reqtest.C)
-                    cout << "请求总量大于剩余所需" << endl;
+                else if (Nt[cursor].A < Reqtest.A && Nt[cursor].B < Reqtest.B && Nt[cursor].C < Reqtest.C)
+                    cout << "REQUEST IS MORE THAN NEED ERROR" << endl;
                 else
                 {
-                    availabe = availabe - Reqtest, Need[cursor] = Need[cursor] - Reqtest, Allocation[cursor] = Allocation[cursor] + Reqtest;
+                    Avt = Avt - Reqtest, Nt[cursor] = Nt[cursor] - Reqtest, At[cursor] = At[cursor] + Reqtest;
+                    flag1 = true;
                 }
             }
             else
             {
                 cout << "Progress " << Reqtest.name << " Not found" << endl;
-                exit(0);
             }
-            securityCheck(availabe, P, Allocation, Need, Reqtest);
-            break;
+            if(flag1)
+                securityCheck(Avt, Pt, At, Nt, Reqtest,size);
         }
         else if (MenuN == 3)
         {
